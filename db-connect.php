e91926cf93f87895dc400e8fb1343a065113a874
REN-CARLOS SENTIE B <sentierencarlos1grian@gmail.com>
2024-06-12 13:03:20 +0100

    Education system control

---
+++ db-connect.php
@@ -0,0 +1,18 @@
+<?php 
+// Hostname
+$host = "education-system-db.c9kqumu24jgr.us-west-2.rds.amazonaws.com";
+// Username
+$uname = "admin";
+// Password
+$pw = "Training123";
+// Database Name
+$dbname = "simple_attendance_db";
+
+try{
+    $conn = new MySQLi($host, $uname, $pw, $dbname);
+}catch(Exception $e){
+    echo "Database Connection Failed: <br>";
+    print_r($e->getMessage());
+    exit;
+}
+?>
